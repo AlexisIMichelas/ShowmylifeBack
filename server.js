3ee9da558dc0d74cca9938e76c6e78d8f9c24240
AMichelas <120266869+AMichelas@users.noreply.github.com>
2024-10-21 10:55:38 +0200

    change cloudinary key

--- server.js
+++ server.js
@@ -63,9 +63,6 @@ require('./app/routes/user.routes')(app);
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
-  
-  // Initialiser la base de données
-  init_db(); // Appel à la fonction d'initialisation
 });
 
 module.exports = app;
